0ebd23d1bc79a9e57e4279b298d7b13be006c385
Jagoum Richmond <jagoum24@gmail.com>
2025-07-28 08:45:43 +0100

    feat: first lab setup for demmo

---
+++ getting_started.md
@@ -0,0 +1,15 @@
+# Plan:
+
+- [ ] create vms to form a cluster
+- [ ] install docker on those vms
+- [ ] install kubernetes on those vms
+- [ ] create a kubernetes cluster
+- [ ] create a kubernetes deployment
+- [ ] create a kubernetes service
+- [ ] create a kubernetes ingress
+- [ ] create a kubernetes config
+- [ ] stop one worker node on the cluster and see what happens
+- [ ] start the worker node again and see what happens
+- [ ] stop one control node and see what happens.
+- [ ] start the control node again and see what happens.
+
---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    multipass = {
+      source  = "larstobi/multipass"
+      version = "1.4.2"
+    }
+  }
+}
+
+resource "multipass_instance" "controller" {
+  name = "controller-node"
+  cpus = 2
+  memory = "2G"
+  disk = "10G"
+}
+
+resource "multipass_instance" "worker" {
+  count = 3
+  name = "worker-${count.index}"
+  cpus = 2
+  memory = "2G"
+  disk = "10G"
+}
+
+output "controller_ip" {
+  value = multipass_instance.controller
+}
+
+output "worker_ips" {
+  value = [for instance in multipass_instance.worker : instance]
+}
+
